e7402da0847ec78fc60cc4d0e4e41ce3e7c10f5d
Levent Orhun Efe <orhunefe93@gmail.com>
2024-07-02 21:37:20 +0300

    final

--- src/main/java/com/workintech/spring17challenge/controller/CourseController.java
+++ src/main/java/com/workintech/spring17challenge/controller/CourseController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin("*") //cors hatasını bu yolla çözebiliyoruz
 @RestController
 @RequestMapping("/courses")
 public class CourseController {
